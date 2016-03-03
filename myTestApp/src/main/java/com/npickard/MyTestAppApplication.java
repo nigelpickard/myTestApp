56014ff6416a5f51f2e863db5977b2fb072322ed
Nigel Pickard <nigel_pickard@yahoo.com>
2016-03-02 19:12:26 -0500

    initial commit

---
+++ myTestApp/src/main/java/com/npickard/MyTestAppApplication.java
@@ -0,0 +1,12 @@
+package com.npickard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTestAppApplication.class, args);
+	}
+}
